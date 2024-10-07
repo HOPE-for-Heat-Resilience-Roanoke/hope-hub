e3b4eb849cb1014ba6b70eb994973b19adb934ea
Nathan Self <nwself@vt.edu>
2024-10-07 13:58:33 -0400

    Add new video to homepage

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -7,6 +7,18 @@
 <section class="flex flex-col md:flex-row gap-8 px-8 md:px-0 py-4 pt-8 justify-evenly">
   <article class="prose">
     <Markdown/>
+
+    
+    <section class="my-8 md:max-w-xl">
+      <h4 class="font-bold my-2">2024 Summary Video of our Project</h4>
+      <iframe width="100%" height="350" src="https://www.youtube.com/embed/WEM5ASM-QXY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>
+
+    <section class="mb-20 md:max-w-xl">
+      <h4 class="font-bold my-2">2023 Video Explaining Our Project Approach</h4>
+      <iframe width="100%" height="350" src="https://www.youtube.com/embed/N5xTqLFCmVs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>
+
   </article>
 
   <aside class="flex flex-col items-center">
@@ -17,8 +29,4 @@
       <img src="/images/nsf.png" alt="NSF logo">
     </a>
   </aside>
-</section>
-
-<section class="pb-20 md:max-w-xl">
-  <iframe width="100%" height="350" src="https://www.youtube.com/embed/N5xTqLFCmVs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </section>
